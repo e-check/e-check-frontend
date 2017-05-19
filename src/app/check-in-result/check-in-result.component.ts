e352e8d709edef4308205b807e07f19784b4c1ed
han.jing <han.jing@rea-group.com>
2017-05-19 13:46:05 +0800

    production environment

---
+++ src/app/check-in-result/check-in-result.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-check-in-result',
+  templateUrl: './check-in-result.component.html',
+  styleUrls: ['./check-in-result.component.css']
+})
+export class CheckInResultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
